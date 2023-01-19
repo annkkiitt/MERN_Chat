fd6fc7bee5cc15f79a0adf18f513bfcb359d91b3
Ankit Rawat <ankit2001rawat@gmail.com>
2023-01-19 12:34:39 +0530

    server

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,22 @@
 const express = require('express');
-const app = express();
+const { chats } = require('./data/data');
+const dotenv = require('dotenv');
 
-app.listen(5000, console.log('Started on server 5000'));
+const app = express();
+dotenv.config();
+
+app.get('/', (req, res) => {
+  res.send('Api is running');
+});
+
+app.get('/api/chat', (req, res) => {
+  res.send(chats);
+});
+
+app.get('/api/chat/:id', (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, console.log(`Started on server ${PORT}`));
